41eb90a40607f509e93c53f591ab713a0630d7eb
Lectral <github@mail.ryju.pl>
2026-03-23 15:33:24 +0100

    test: github action

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 import { SAUCE_DEMO_BASE_URL } from './tests/test-data/saucedemo.constants';
 
 const headedInDocker = process.env.PW_HEADED === '1';
+const reporter = process.env.CI
+  ? [['github'], ['html', { open: 'never', outputFolder: '.artifacts/playwright-report' }], ['list']]
+  : [['html', { open: 'never', outputFolder: '.artifacts/playwright-report' }], ['list']];
 
 export default defineConfig({
   testDir: './tests/specs',
@@ -11,11 +14,11 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [['html', { open: 'never', outputFolder: '.artifacts/playwright-report' }], ['list']],
+  reporter,
   use: {
     baseURL: SAUCE_DEMO_BASE_URL,
     testIdAttribute: 'data-test',
-    trace: 'on-first-retry',
+    trace: process.env.CI ? 'retain-on-failure' : 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     headless: headedInDocker ? false : true,
